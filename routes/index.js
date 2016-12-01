91a5893663757ed77bfd996e2435ee83fe730ecc
mattx264 <mattx264@yahoo.com>
2016-12-01 00:12:21 -0600

    js update

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
- 
-  res.render('index', { portfolio: {} });
+  var MongoClient = require('mongodb').MongoClient
+  var URL = 'mongodb://mmadmin:mmachaj123@ds061076.mlab.com:61076/mmachaj'
+  MongoClient.connect(URL, function (err, db) {
+    if (err) return
+
+    var collection = db.collection('portfolio');
+   
+    collection.find().toArray(function (err, docs) {
+
+      res.render('index', { portfolio: docs });
+    })
+
+    db.close()
+  });
+ // res.render('index', { portfolio: docs });
 
 });
 
