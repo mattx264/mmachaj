d1b7e6fde414c96195ef70e7007dd7328d557d9e
mattx264 <mattx264@yahoo.com>
2016-12-03 00:53:57 -0600

    clean up

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var MongoClient = require('mongodb').MongoClient
-  var URL = process.env.CONNECTION_STRING 
-  MongoClient.connect(URL, function (err, db) {
-    if (err) return
-
-    var collection = db.collection('portfolio');
+  /*var MongoClient = require('mongodb').MongoClient
+  var URL = process.env.CONNECTION_STRING;
+  if (URL !== undefined) {
    
-    collection.find().toArray(function (err, docs) {
+    MongoClient.connect(URL, function (err, db) {
+      if (err) return
 
-      res.render('index', { portfolio: docs });
-    })
+      var collection = db.collection('portfolio');
 
-    db.close()
-  });
-  //res.render('index', { portfolio: {} });
+      collection.find().toArray(function (err, docs) {
+
+        res.render('index', { portfolio: docs });
+      })
+
+      db.close()
+    });
+  }*/
+  res.render('index', { portfolio: {} });
 
 });
 
